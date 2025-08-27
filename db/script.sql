c900dd9ef6be53919f21f83fa2b2d035d05a6f3a
LuyerPerez <luyerperez0@gmail.com>
2025-08-26 23:35:34 -0500

    Sprint 1: Login y registro 100%

--- db/script.sql
+++ db/script.sql
@@ -39,7 +39,6 @@ CREATE TABLE `calificacion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
-
 --
 -- Estructura de tabla para la tabla `notificacion`
 --
